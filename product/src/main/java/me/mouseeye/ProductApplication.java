3fbab6a9eb68829997105274163bd9a6c959bd6e
mouseeye <mouseeye@mouseeyes-MacBook-Air.local>
2023-05-24 14:00:32 +0900

    instrumentation

--- product/src/main/java/me/mouseeye/ProductApplication.java
+++ product/src/main/java/me/mouseeye/ProductApplication.java
@@ -2,16 +2,17 @@ package me.mouseeye;
 
 import com.apollographql.federation.graphqljava.tracing.FederatedTracingInstrumentation;
 import graphql.execution.instrumentation.Instrumentation;
+import graphql.execution.instrumentation.tracing.TracingInstrumentation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class PddApplication {
+public class ProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PddApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 
     @Bean
